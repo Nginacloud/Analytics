0cbf72201d3e4a416779b41a5090f611d6ea6f0a
unknown <Ngina Cloud>
2025-08-18 17:35:48 +0300

    progress

--- Apacheairflow/products2.py
+++ Apacheairflow/products2.py
@@ -2,26 +2,28 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-#url = "https://zucchini.co.ke/collections/vegetables"
-url = "https://zucchini.co.ke/"
+url = "https://naivas.online/"
+
 #requests.get(url)
 response = requests.get(url)
-#print(response)
+print(response.text)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-#print(soup)
-products = soup.find_all('div', class_='wbproductdes card__content')
-
-with open("products21.csv", "w", newline="", encoding="utf-8") as productfile:
+print(soup)
+products = soup.find_all('div', class_='h-full')
+if not products:
+    print("No product containers found. Check the class name.")
+    exit()
+with open("product.csv", "w", newline="", encoding="utf-8") as productfile:
     fieldnames = ['Name', 'Price']
     writer = csv.DictWriter(productfile, fieldnames=fieldnames)
     writer.writeheader()
     
     for products in products:
-        name_element = products.find('h3', class_='product-title')
+        name_element = products.find('a', class_='!text-naivas-gray-dark')
         name = name_element.text.strip() if name_element else 'N/A'
 
-        price_element = products.find('span', class_='price-item--sale')
+        price_element = products.find('span', class_='font-bold text-naivas-green')
         price = price_element.text.strip() if price_element else 'N/A'
 
         writer.writerow({'Name': name, 'Price': price})
